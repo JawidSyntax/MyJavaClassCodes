f73e2cf16a4c2887f3904151a1c2763f3ceb4b2f
JawidSyntax <enayatjawid@gmail.com>
2019-03-28 23:46:36 -0400

    HomeWorks

--- src/com/class13/Task1Reverse.java
+++ src/com/class13/Task1Reverse.java
@@ -4,16 +4,20 @@ public class Task1Reverse {
 	
 	public static void main(String[] args) {
 		
-		String name = "Sunday";
 		
-		for (int i=name.length()-1; i>=0; i--) {
-			System.out.print(name.charAt(i));
-		}
-		
-		
-		
-	}
-	}
+		int[] zip = new int[5];
+
+		zip[0] = 7;
+		zip[1] = 3;
+		zip[2] = 4;
+		zip[3] = 1;
+		zip[4] = 9;
+
+		System.out.println( zip[ 2 + 1 ] );
+		}}
+
+
 	
- 
+	
+
 
--- src/com/class13/Task3Names.java
+++ src/com/class13/Task3Names.java
@@ -1,5 +1,47 @@
 package com.class13;
 
-public class Task3Names {
+import java.util.Scanner;
 
-}
+public class Task3Names {
+	
+	public static void main(String[] args) {
+		
+		/* Write a program that reads two people’s first names and if they expecting boy or girl? Based on the input suggests a name for a baby:
+Example Output:
+Mom’s first name? Mary
+Dad’s first name? Daniel
+Boy or Girl? boy
+Suggested baby name: DANRY
+
+Example Output:
+Mom’s first name? Mary
+Dad’s first name? Daniel
+Boy or Girl? girl
+Suggested baby name: MAIEL */
+		
+		String mom, dad, child;
+		String  gender ;
+		Scanner input = new  Scanner(System.in);
+		System.out.println("What is Mom's first name?");
+		mom = input.nextLine();
+		
+		
+		System.out.println("What is Dad's first name?");
+		dad = input.nextLine();
+		
+		System.out.println("What do you expect?");
+		gender = input.nextLine();
+
+		if(gender.equals ("boy")) {
+			System.out.println("Adam");
+		}
+		if (gender.equals ("girl")) {
+				System.out.println("Marry");
+		} else {
+			System.out.println("Please identfy the gender");
+		}
+			
+		}
+	}
+
+
