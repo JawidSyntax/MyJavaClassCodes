a3afbf57b2aab0944675dc681065e6b9272667d3
JawidSyntax <enayatjawid@gmail.com>
2019-04-04 19:36:47 -0400

    New Codes

---
+++ src/com/class17/NoMainMethod.java
@@ -0,0 +1,24 @@
+package com.class17;
+
+public class NoMainMethod {
+	
+	String str;
+	
+	int num;
+	
+	void hello() {
+		System.out.println("Hello");
+	}
+	void bye() {
+		System.out.println("Bye");
+	}
+	 public static void main (String [] args) {
+		 
+		 NoMainMethod obj = new  NoMainMethod();
+		 obj.hello();
+		 
+		 
+	 }
+	}
+
+
